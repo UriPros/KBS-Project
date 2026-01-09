e2cbbeadf2b2ae2cd1ecdc7605699a624de60039
sgibertm <sara.gibertm@gmail.com>
2026-01-09 23:15:03 +0100

    Enhance recommendation logic with weather label
    Added weather label extraction to improve recommendations.

--- inference_engine.py
+++ inference_engine.py
@@ -1,7 +1,8 @@
 import json
-from fuzzification import fuzzify_user_input
+from fuzzification import fuzzify_user_input, extract_weather_label
 from rule_evaluation import evaluate_rules
 from defuzzification import defuzzify
+
 
 
 def recommend_destination(user_input):
@@ -15,10 +16,21 @@ def recommend_destination(user_input):
         fuzzy_sets = json.load(f)
 
     fuzzified_inputs = fuzzify_user_input(user_input, fuzzy_sets) #fuzzification
+    fuzzified_inputs = fuzzify_user_input(user_input, fuzzy_sets)
+
+    # OPTIONAL, ho he posat just TO CHCK: print("FUZZIFIED INPUTS:", fuzzified_inputs)
+
+    weather_label = extract_weather_label(fuzzified_inputs)
+    if weather_label:
+        fuzzified_inputs["weather"] = {weather_label: 1.0}
+
 
     scores = evaluate_rules(fuzzified_inputs, rules, destinations) # rule evaluation
 
     def_scores = defuzzify(scores) # defuzzification
+
+    if not def_scores:
+        return None, {}
 
     best = max(def_scores, key=def_scores.get) #choose best destination
 
@@ -26,12 +38,15 @@ def recommend_destination(user_input):
 
 if __name__ == "__main__":
     user_input = {
-        "temperature": 0.2,
-        "budget": 0.6,
-        "eco_friendly": 0.6
+        "avg_temperature": 20,
+        "budget": 150,
+        "eco_friendly": 0.8
     }
+
 
     best, scores = recommend_destination(user_input)
 
     print("Recommended destination:", best)
     print("Scores:", dict(scores))
+
+
