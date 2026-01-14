7ee3f4fb7941fbe3d89d70e17b055064c7ff406a
UriPros <loiropros@gmail.com>
2026-01-14 01:36:06 +0100

    ACABAT

--- defuzzification.py
+++ defuzzification.py
@@ -1,7 +1,7 @@
 def defuzzify_destination(scores):
     if not scores:
         return 0.0
-    return max(scores.values()) #ho podem canviar perque retorni el que vulguem
+    return sum(scores.values()) / len(scores) #ho podem canviar perque retorni el que vulguem
 
 
 def defuzzify(scores):
@@ -9,7 +9,16 @@ def defuzzify(scores):
     final_scores = {}
 
     for destination, fuzzy_scores in scores.items():
-
         final_scores[destination] = defuzzify_destination(fuzzy_scores)
 
-    return final_scores
+    destinations_list = list(final_scores.items())
+
+    # Sort the list by the score from highest to lowest
+    destinations_list.sort(key=lambda x: x[1], reverse=True)
+
+    top3_list = destinations_list[:3]
+
+    top3_scores = {destination: score for destination, score in top3_list}
+
+
+    return top3_scores
